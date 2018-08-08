cab1574b5c0b36eb32aca5a7de8934ead02c8559
vizziato <gidm700@gmail.com>
2018-08-08 08:18:12 +0200

    add array

--- src/components/crud-firebase/crud-firebase.ts
+++ src/components/crud-firebase/crud-firebase.ts
@@ -43,7 +43,7 @@ export class CrudFirebaseComponent implements OnInit {
 
   item:any;
 
-  songsItem:any;
+  public songsItem:Array<any> = [];
 
 
  
@@ -61,7 +61,7 @@ export class CrudFirebaseComponent implements OnInit {
     this.documentsRef = this.af.list(that.itemsRef+that.itemRef);
     this.documentsQuery = this.documentsRef.query.limitToFirst(3);
     
-
+    
     console.log('this.rootRef',this.rootRef);
     console.log('this.documentsRef ',this.documentsRef )
     console.log('this.documentsQuery',this.documentsQuery)
@@ -75,10 +75,26 @@ export class CrudFirebaseComponent implements OnInit {
       name: ['test'],
       quantity: 0
     });
+
+    /*this.songsItem =({
+      id:'',
+      title:''
+    })*/
   
    console.log('this.documentsForm',this.documentsForm.controls.name.value);
-
+   var count =0;
+  
     this.af.list('/songs').query.on("child_added",function(snapshot) {
+    count++
+    console.log('var count',count);
+
+    var newValue = snapshot.val();
+    //that.songsItem[count] = snapshot.toJSON();
+    that.songsItem.push(snapshot.toJSON());
+
+  
+    console.log(newValue);
+    console.log('that.songsItem',that.songsItem);
     // This will be called exactly two times (unless there are less than two
     // dinosaurs in the Database).
   
@@ -86,10 +102,19 @@ export class CrudFirebaseComponent implements OnInit {
     // removed from the data set, as a new dinosaur will now be the second
     // shortest.
     //that.songs.snapshotChanges. = snapshot.key
-    console.log('child_added songs snapshot.key',snapshot);
-
+    console.log('child_added songs snapshot ',snapshot);
+    console.log('child_added songs snapshot val ',snapshot.val());
+    console.log('child_added songs snapshot val ',snapshot.toJSON());
+      //that.songsItem = snapshot.toJSON()
+      /*snapshot.forEach(actions=>{
+        var test = actions.val().id;
+       console.log('test',test);
+      console.info('this.songsItem',that.songsItem)
+      });*/
     console.log('child_added songs snapshot.key',snapshot.key);
   });
+
+  
 
 }
 
@@ -99,9 +124,9 @@ export class CrudFirebaseComponent implements OnInit {
 
     this.songsRef = this.af.list('/songs');
     // Use snapshotChanges().map() to store the key
-    this.songsItem = this.songsRef.snapshotChanges().map(changes => {
+    /*this.songsItem = this.songsRef.snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-    });
+    });*/
 
    
 
@@ -110,6 +135,8 @@ export class CrudFirebaseComponent implements OnInit {
 
       //this.items$ = this.af.list('/items')
     console.info(this.items)
+
+    console.info('this.songsItem',this.songsItem)
 
 
     console.info(this.items.query.on("child_added", function(snapshot) {
@@ -280,6 +307,7 @@ export class CrudFirebaseComponent implements OnInit {
     this.af.list(this.itemRef+count).push({name: name, quantity: quantity });
     //this.items.push({name: name, quantity: quantity });
     //this.itemValue ='';
+    
     
   }
   addItem(name,quantity): void {
